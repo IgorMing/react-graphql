643188114b9d83195300b2a29a2a91741ae9cbe0
Igor Ming de Mesquita <igor.mesquita@s2it.com.br>
2018-04-27 19:18:04 -0300

    Initial commit (just boilerplate)

---
+++ src/pages/Home/index.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+import { logoutAction } from 'components/Auth/duck';
+
+import './index.css';
+import { callFlow, resetFlow } from './duck';
+
+const mapStateToProps = ({ home }) => ({ ...home });
+const mapDispatchToProps = { callFlow, resetFlow, logout: logoutAction };
+
+@connect(mapStateToProps, mapDispatchToProps)
+export default class Home extends Component {
+  static propTypes = {
+    callFlow: PropTypes.func.isRequired,
+    calls: PropTypes.number.isRequired,
+    logout: PropTypes.func.isRequired,
+    resetFlow: PropTypes.func.isRequired
+  }
+
+  callRedux = () => {
+    this.props.callFlow();
+  }
+
+  reset = () => {
+    this.props.resetFlow();
+  }
+
+  render() {
+    const { calls } = this.props;
+
+    return (
+      <div>
+        <div className="centered">
+          Home screen
+        </div>
+
+        <div className="flex-column action-container">
+          <span>Redux flow is called {calls} times.</span>
+          <button onClick={this.callRedux}>Call redux</button>
+          <button onClick={this.reset}>Reset Flow</button>
+        </div>
+
+        <button onClick={this.props.logout}>Logout</button>
+      </div>
+    );
+  }
+}
---
+++ src/pages/Login/index.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+import { loginAction } from 'components/Auth/duck';
+import LoginForm from 'components/LoginForm';
+
+const mapDispatchToProps = { login: loginAction };
+
+@connect(null, mapDispatchToProps)
+export default class Login extends Component {
+  static propTypes = {
+    login: PropTypes.func.isRequired
+  }
+
+  onSubmit = (event) => {
+    event.preventDefault();
+    this.props.login('user_test', 'password');
+  }
+
+  render() {
+    return <LoginForm onSubmit={this.onSubmit} />;
+  }
+}
